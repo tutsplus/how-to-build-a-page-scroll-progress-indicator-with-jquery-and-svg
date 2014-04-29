9e56895a4030e2b5d3f420ca38eab71a968aae4b
Ian Yates <ian@snaptin.com>
2014-04-29 18:27:51 +0200

    updated for screen resize

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 (function(){
-	var $w = $(window);
+    var $w = $(window);
 	var $circ = $('.animated-circle');
 	var $progCount = $('.progress-count');
 	var $prog2 = $('.progress-indicator-2');
-	var wh = $w.height();
-	var h = $('body').height();
-	var sHeight = h - wh;
+
+	var wh, h, sHeight;
+
+	function setSizes(){
+		wh = $w.height();
+		h = $('body').height();
+		sHeight = h - wh;
+	}
+
+	setSizes();
+
 	$w.on('scroll', function(){
 		var perc = Math.max(0, Math.min(1, $w.scrollTop()/sHeight));
 		updateProgress(perc);
+	}).on('resize', function(){
+		setSizes();
+		$w.trigger('scroll');
 	});
 
 	function updateProgress(perc){
